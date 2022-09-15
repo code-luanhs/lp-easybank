2d44c3517d524a6704e3dba2e666cd08e22da633
Luan Henrique <luan.henrique.black@gmail.com>
2022-09-15 19:56:28 -0300

    commit inicial

---
+++ src/util/theme.ts
@@ -0,0 +1,21 @@
+export const THEME = {
+    COLORS: {
+        DARK_BLUE: 'hsl(233, 26%, 24%)',
+        LIME_GREEN: 'hsl(136, 65%, 51%)',
+        BRIGHT_CIAN: 'hsl(192, 70%, 51%)',
+        GRAYISH_BLUE: 'hsl(233, 8%, 62%)',
+        LIGHT_GRAYISH_BLUE: 'hsl(220, 16%, 96%)',
+        VERY_LIGHT: 'hsl(0, 0%, 98%)',
+        WHITE: 'hsl(0, 0%, 100%)'
+    },
+    FONTS: {
+        SIZE: {
+            BODY: 18
+        },
+        WEIGHT: {
+            LIGHT: 300,
+            REGULAR: 400,
+            BOLD: 700
+        }
+    }
+}
