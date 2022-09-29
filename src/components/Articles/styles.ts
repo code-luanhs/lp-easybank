fe6736ccdbc4241a07d84c65b7d2a41ec32936ae
Pablo <pablo.kalyell.441@gmail.com>
2022-09-29 14:01:35 -0300

    modificação artigos

--- src/components/Articles/styles.ts
+++ src/components/Articles/styles.ts
@@ -4,6 +4,9 @@ import { THEME } from "../../util/theme";
 export const Title = styled.h1`
   margin-left: 55px;
   font-weight: ${THEME.FONTS.WEIGHT.REGULAR};
+  @media (max-width: 770px) {
+   margin-left:0;
+  }
 `;
 export const Container = styled.div`
   background: ${THEME.COLORS.VERY_LIGHT};
@@ -16,6 +19,10 @@ export const Swapper = styled.div`
   gap: 2.3rem;
   margin-left:50px;
   margin-right:50px;
+  @media (max-width: 770px) {
+   margin-left:15px;
+   margin-right:15px;
+  }
 `;
 export const TitleDiv = styled.div`
   margin: 0;
