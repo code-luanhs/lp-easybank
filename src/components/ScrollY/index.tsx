493a28fe1e0e5756b753cbf709d3a6d50bcd4e44
Pablo <pablo.kalyell.441@gmail.com>
2022-10-04 12:00:02 -0300

    svg button top

--- src/components/ScrollY/index.tsx
+++ src/components/ScrollY/index.tsx
@@ -1,38 +1,37 @@
-import React, {useState} from 'react';
-import {BtnUp,IconTop} from './styles'
-  
-const ScrollButton = () =>{
-  
-  const [visible, setVisible] = useState(false)
-  
+import React, { useState } from "react";
+import { BtnUp, IconTop } from "./styles";
+
+const ScrollButton = () => {
+  const [visible, setVisible] = useState(false);
+
   const toggleVisible = () => {
     const scrolled = document.documentElement.scrollTop;
-    if (scrolled > 300){
-      setVisible(true)
-    } 
-    else if (scrolled <= 300){
-      setVisible(false)
-    }
-    else if (scrolled >= 300){
-      setVisible(false)
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    } else if (scrolled >= 300) {
+      setVisible(false);
     }
   };
-  
-  const scrollToTop = () =>{
+
+  const scrollToTop = () => {
     window.scrollTo({
-      top: 0, 
-      behavior:'smooth'
+      top: 0,
+      behavior: "smooth",
     });
   };
-  
-  window.addEventListener('scroll', toggleVisible);
-  
+
+  window.addEventListener("scroll", toggleVisible);
+
   return (
     <BtnUp>
-     <IconTop onClick={scrollToTop} 
-     style={{display: visible ? 'inline' : 'none'}} />
+      <IconTop
+        onClick={scrollToTop}
+        style={{ display: visible ? "inline" : "none" }}
+      />
     </BtnUp>
   );
-}
-  
+};
+
 export default ScrollButton;
--- src/components/ScrollY/styles.ts
+++ src/components/ScrollY/styles.ts
@@ -1,24 +1,23 @@
-import { RiArrowUpCircleLine } from 'react-icons/ri';
-import styled from 'styled-components';
-import { THEME } from '../../util/theme';
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
+import styled from "styled-components";
+import { THEME } from "../../util/theme";
 
 export const BtnUp = styled.a`
-  position:fixed;
-bottom:9rem;
-left:97%;
-   z-index: 1;
-   cursor: pointer;
-   transition: ${THEME.TRANSITION.DEFAULT};
-   @media (max-width: 770px) {
-    left:50%;
-    display:none;
+  position: fixed;
+  bottom: 9rem;
+  left: 97%;
+  z-index: 1;
+  cursor: pointer;
+  transition: ${THEME.TRANSITION.DEFAULT};
+  @media (max-width: 770px) {
+    left: 50%;
+    display: none;
   }
 `;
 
-export const IconTop = styled(RiArrowUpCircleLine)`
-height:40px;
-width: 40px;
-color: ${THEME.COLORS.LIME_GREEN};
-cursor:pointer;
-
+export const IconTop = styled(BsFillArrowUpCircleFill)`
+  height: 40px;
+  width: 40px;
+  cursor: pointer;
+  color: #2bc5c5;
 `;
